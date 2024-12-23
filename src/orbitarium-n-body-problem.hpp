f9c3e34573ce34207499b7c129b95937a49b0af3
dinnarus <99605736+Malek-Dinari@users.noreply.github.com>
2024-12-23 01:09:28 +0100

    intial project main header added

---
+++ src/orbitarium-n-body-problem.hpp
@@ -0,0 +1,8 @@
+﻿// orbitarium-n-body-problem.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma warning(disable:4275)
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
